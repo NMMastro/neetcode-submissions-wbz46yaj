2c3a3f6cb6d8aab38eb457d9439ad5517c26333d
Nate Mastronardi <77860554+NMMastro@users.noreply.github.com>
2026-04-08 21:55:30 -0400

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        # Find upper bound to k
+        low = 1
+        high = max(piles)
+        k = -1
+
+        # Binary Search to find ideal value of k
+
+        while low <= high:
+            k = math.floor((high + low) / 2)
+
+            time = 0
+            for p in piles:
+                time += math.ceil(p / k)
+
+                if time > h:
+                    break
+
+            if time > h:
+                low = k + 1
+            elif high == k:
+                return k
+            else:
+                high = k
+
+        return None
+
+            
